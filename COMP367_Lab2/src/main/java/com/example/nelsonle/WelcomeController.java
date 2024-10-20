c6b31dc222d4a271383eb584757443b1ce66351b
nelsonle39 <nle39@my.centennialcollege.ca>
2024-10-19 22:03:47 -0400

    Update WelcomeController.java
    For step 6

--- COMP367_Lab2/src/main/java/com/example/nelsonle/WelcomeController.java
+++ COMP367_Lab2/src/main/java/com/example/nelsonle/WelcomeController.java
@@ -1,13 +1,18 @@
 package com.example.nelsonle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import java.time.LocalTime;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/")
-    public String welcome() {
-        return "Welcome to COMP367";
-    }
+	@GetMapping("/")
+	public String welcome() {
+	    LocalTime currentTime = LocalTime.now();
+	    String greeting = (currentTime.isBefore(LocalTime.NOON)) 
+	        ? "Good morning, Nelson, Welcome to COMP367"
+	        : "Good afternoon, Nelson, Welcome to COMP367";
+	    return greeting;
+	}
 }
