b55c80720f05f5cb46b66a08e31ef10a5e70f8b1
Nelson Le <nle39@my.centennialcollege.ca>
2024-10-19 20:17:13 -0400

    Initial commit for COMP367 Lab2

---
+++ COMP367_Lab2/src/test/java/com/example/nelsonle/Comp367Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nelsonle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp367Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
