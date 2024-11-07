3f291e77b52a8f12c9ddd5f323b9d960ccae26fe
Nelson Le <nelsonle345@gmail.com>
2024-11-06 19:46:36 -0500

    Merged

--- COMP367_Lab2/src/main/java/com/example/nelsonle/WelcomeController.java
+++ COMP367_Lab2/src/main/java/com/example/nelsonle/WelcomeController.java
@@ -6,26 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	//Adding this comment to test auto trigger
-	@GetMapping("/")
-	public String welcome() {
-	    LocalTime currentTime = LocalTime.now();
-	    String greeting = (currentTime.isBefore(LocalTime.NOON)) 
-	        ? "Good morning, Nelson, Welcome to COMP367"
-	        : "Good afternoon, Nelson, Welcome to COMP367";
-	    return greeting;
-	}
-
-<<<<<<< HEAD
-	@GetMapping("/")
-	public String welcome() {
-	    LocalTime currentTime = LocalTime.now();
-	    String greeting = (currentTime.isBefore(LocalTime.NOON)) 
-	        ? "Good morning, Nelson, Welcome to COMP367"
-	        : "Good afternoon, Nelson, Welcome to COMP367";
-	    return greeting;
-	}
-=======
-	
->>>>>>> branch 'master' of https://github.com/nelsonle39/lab2.git
+    
+    @GetMapping("/")
+    public String welcome() {
+        LocalTime currentTime = LocalTime.now();
+        String greeting = (currentTime.isBefore(LocalTime.NOON)) 
+            ? "Good morning, Nelson, Welcome to COMP367"
+            : "Good afternoon, Nelson, Welcome to COMP367";
+        return greeting;
+    }
 }
